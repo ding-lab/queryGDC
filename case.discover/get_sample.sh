a541545a0b58f07fb3937e1d1198e0d963cc40d9
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-12-27 12:17:53 -0600

    Adding features
    * Will not retry queries on fatal errors
    * Token can be passed as environment variable

--- case.discover/get_sample.sh
+++ case.discover/get_sample.sh
@@ -1,14 +1,27 @@
 # Return details on all samples for a given case
 # writes "sample_from_case" file
 
-if [ "$#" -ne 2 ]; then
-    echo Error: Wrong number of arguments
-    echo Usage: get_sample.sh CASE TOKEN
-    exit
+# Environment variable QUERYGDC_HOME points queryGDC installation dir
+# default is ./queryGDC
+
+# TODO: introduce arguments here
+# GDC_TOKEN logic should be similar to that in queryGDC - can be set with -t or environment variable
+# currently, token is not passed but read from GDC_TOKEN
+
+
+if [ "$#" -ne 1 ]; then
+    >&2 echo Error: Wrong number of arguments
+    >&2 echo Usage: get_sample.sh CASE 
+    exit 0
 fi
 
 CASE=$1
-TOKEN=$2
+
+if [ -z $GDC_TOKEN ]; then
+    >&2 GDC_TOKEN environment variable not defined.  Quitting.
+    exit 1
+fi
+ 
 
 function sample_from_case_query {
 CASE=$1 # E.g C3L-00004
@@ -24,6 +37,13 @@ cat <<EOF
 EOF
 }
 
+if [ -z $QUERYGDC_HOME ]; then
+QUERYGDC_HOME="./queryGDC"
+>&2 echo QUERYGDC_HOME not set, using default ./queryGDC
+fi
+QUERYGDC="$QUERYGDC_HOME/queryGDC"
+
+
 OUTD="dat/$CASE"
 mkdir -p $OUTD
 OUT="$OUTD/sample_from_case.$CASE.dat"
@@ -32,7 +52,8 @@ Q=$(sample_from_case_query $CASE)
 
 >&2 echo QUERY: $Q
 
-R=$(echo $Q | queryGDC -r -v -t $TOKEN -)
+#R=$(echo $Q | queryGDC -r -v -t $TOKEN -)
+R=$(echo $Q | $QUERYGDC -r -v -)
 
 echo $R | jq -r '.data.sample[] | "\(.submitter_id)\t\(.id)\t\(.sample_type)"' > $OUT
 
