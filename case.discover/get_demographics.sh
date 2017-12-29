680106d248da78ebce20e379713ad01f71ea1b73
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-12-29 12:09:57 -0600

    Updating get_demographics

--- case.discover/get_demographics.sh
+++ case.discover/get_demographics.sh
@@ -1,14 +1,17 @@
 # Get demographics information about all cases in given cases file
 # Writes one line per CASE to stdout
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
     echo Error: Wrong number of arguments
-    echo Usage: get_demographics.sh CASE_FILE TOKEN_FILE
+    echo Usage: get_demographics.sh CASE_FILE 
     exit
 fi
 
 CASES=$1
-TOKEN=$2
+if [ -z $GDC_TOKEN ]; then
+    >&2 GDC_TOKEN environment variable not defined.  Quitting.
+    exit 1
+fi
 
 function demo_from_case_query {
 CASE=$1 # E.g C3L-00004
@@ -25,6 +28,12 @@ cat <<EOF
 EOF
 }
 
+if [ -z $QUERYGDC_HOME ]; then
+    QUERYGDC_HOME="./queryGDC"
+    >&2 echo QUERYGDC_HOME not set, using default ./queryGDC
+fi
+QUERYGDC="$QUERYGDC_HOME/queryGDC"
+
 # print header
 printf "# case\tethnicity\tgender\trace\tdays_to_birth\n"
 
@@ -34,7 +43,7 @@ while read CASE; do
     [[ $CASE = \#* ]] && continue  # Skip commented out entries
 
     Q=$(demo_from_case_query $CASE)
-    R=$(echo $Q | queryGDC -r -t $TOKEN -)
+    R=$(echo $Q | $QUERYGDC -r -)
     LINE=$(echo $R | jq -r '.data.demographic[] | "\(.ethnicity)\t\(.gender)\t\(.race)\t\(.days_to_birth)"')
 
     printf "$CASE\t$LINE\n" 
